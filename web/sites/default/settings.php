dd0fe707971fc19a71cd2923b6cc58ec36f32487
rloos289 <rloos289@gmail.com>
2024-12-04 15:39:19 -0800

    update settings.php

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -32,18 +32,42 @@ $local_settings = __DIR__ . "/settings.local.php";
 if (file_exists($local_settings)) {
   include $local_settings;
 }
-$databases['default']['default'] = array (
-  'database' => 'drupal9',
-  'username' => 'drupal9',
-  'password' => 'drupal9',
-  'prefix' => '',
-  'host' => 'database',
-  'port' => '3306',
-  'isolation_level' => 'READ COMMITTED',
-  'driver' => 'mysql',
-  'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
-  'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
-);
+
 $settings['hash_salt'] = 'n2poBzyIBB-Dh_8QqZI3Lak4159yFw3uWxDcGqisZ9ik9Ptg-O-OoI68ngaYUpOsy4qphPmB_w';
 
 $settings['config_sync_directory'] = '../config/default';
+
+$_env_is_pantheon = TRUE;
+// Local settings.
+if (isset($_ENV['LANDO_INFO'])) {
+    $_env_is_pantheon = FALSE;
+    $lando_info = json_decode(getenv('LANDO_INFO'), TRUE);
+    $databases['default']['default'] = [
+      'database' => $lando_info['database']['creds']['database'],
+      'username' => $lando_info['database']['creds']['user'],
+      'password' => $lando_info['database']['creds']['password'],
+      'host' => $lando_info['database']['internal_connection']['host'],
+      'port' => $lando_info['database']['internal_connection']['port'],
+      'driver' => 'mysql',
+      'prefix' => '',
+      'collation' => 'utf8mb4_general_ci',
+    ];
+    unset($lando_info);
+
+  $settings['file_private_path'] = 'sites/default/private';
+  $settings['skip_permissions_hardening'] = TRUE;
+
+  $config['system.performance']['css']['preprocess'] = FALSE;
+  $config['system.performance']['js']['preprocess'] = FALSE;
+  $config['system.logging']['error_level'] = 'some';
+
+  $settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
+  $settings['cache']['bins']['render'] = 'cache.backend.null';
+  $settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
+
+  // Include local settings last so that they can override anything.
+  if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+    include $app_root . '/' . $site_path . '/settings.local.php';
+  }
+}
+
